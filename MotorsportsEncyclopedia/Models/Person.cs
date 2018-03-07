192d94cbfee627b63407615c766f1904adeb9a2f
thejai riem <rthejai@uw.edu>
2018-03-07 03:22:17 -0800

    1st commit

---
+++ MotorsportsEncyclopedia/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MotorsportsEncyclopedia.Models
+{
+	public class Person
+	{
+		public int ID { get; set; }
+
+		[StringLength(100, ErrorMessage = "Last name cannot be longer than 100 characters.")]
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
+
+		[StringLength(100, ErrorMessage = "First name cannot be longer than 100 characters.")]
+		[Column("FirstName")]
+		[Display(Name = "First Name")]
+		public string FirstMidName { get; set; }
+
+		[Display(Name = "Full Name")]
+		public string FullName
+		{
+			get
+			{
+				return LastName + ", " + FirstMidName;
+			}
+		}
+	}
+}
